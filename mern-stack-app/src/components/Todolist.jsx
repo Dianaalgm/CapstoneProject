62da07ee266b6b037b2a574b3de0af0ca65e7145
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-05-29 12:00:23 -0400

    commmit

--- mern-stack-app/src/components/Todolist.jsx
+++ mern-stack-app/src/components/Todolist.jsx
@@ -19,23 +19,57 @@ const Todolist = () => {
       fetchTodos();
     }, []);
   
+    const deleteTodo = async (id) => {
+      try {
+        await axios.delete(`http://localhost:3000/todo/${id}`);
+        setTodos((prevTodos) => prevTodos.filter(todo => todo._id !== id));
+        console.log(`Todo with id ${id} deleted`);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+  
     return (
-      <div className="todoContainer">
-        <h3>This is the To Do List Component</h3>
-        {todos.length > 0 ? (
-          <ul>
-            {todos.map((todo, index) => (
-              <li key={index}>{todo.title}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No to-dos available</p>
-        )}
-      </div>
-    );
-  };
+        <div className="todoContainer">
+          <h2>To Do List</h2>
+          <table className="table">
+            <thead className="thead-light">
+              <tr>
+                {/* <th>User</th> */}
+                <th>Task</th>
+                <th>Completed</th>
+                <th>Date</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody className="tbodyContainer">
+              {todos.length > 0 ? (
+                todos.map((todo) => (
+                  <tr key={todo._id}>
+                    {/* <td>{todo.user}</td> */}
+                    <td>{todo.title}</td>
+                    <td>{todo.completed ? 'Yes' : 'No'}</td>
+                    <td>{todo.createdAt}</td>
+                    <td>
+                      <button onClick={() => deleteTodo(todo._id)}>Done</button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4">No to-dos available</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      );
+    };
+    
   
   export default Todolist;
+
+
 // import React from 'react';
 
 // const ToDoList = () => (
