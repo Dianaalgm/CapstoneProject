e995be42fedfd7284eaf2ca7a0c5f588f9abc3f1
Diana Garcia-Martinez <dianagarcia1316@gmail.com>
2024-05-29 11:17:01 -0400

    To Do List is connected to Db

--- mern-stack-app/src/components/Todolist.jsx
+++ mern-stack-app/src/components/Todolist.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
 
 const Todolist = () => {
+    const [todos, setTodos] = useState([]);
+  
+    useEffect(() => {
+      const fetchTodos = async () => {
+        try {
+          const res = await axios.get('http://localhost:3000/todo/');
+          setTodos(res.data);
+          console.log(res.data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+  
+      fetchTodos();
+    }, []);
+  
     return (
-        <div>
-            <p>This is the To Do List Component</p>
-        </div>
+      <div className="todoContainer">
+        <h3>This is the To Do List Component</h3>
+        {todos.length > 0 ? (
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>{todo.title}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No to-dos available</p>
+        )}
+      </div>
     );
-};
-
-export default Todolist;
-
-
+  };
+  
+  export default Todolist;
 // import React from 'react';
 
 // const ToDoList = () => (
